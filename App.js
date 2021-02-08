6644feac200b1dd66877a6a80438e469fbec39d1
bini0823 <bini0823@cau.ac.kr>
2021-02-08 19:48:37 +0900

    stack, bottomTab 네비게이션을 통한 Screen들 Render

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
   SafeAreaView,
@@ -15,100 +7,57 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import * as screens from './src/screens/index';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+const Stack = createStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+const App = () => {
+  const {Intro, SignUp, MyNote, Map, Write, Post, Setting} = screens;
+  const Navigator = () => {
+    const loginStack = () => {
+      return (
+        <Stack.Navigator initialRouteName="Intro" headerMode="false">
+          <Stack.Screen name="Intro" component={Intro} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+        </Stack.Navigator>
+      );
+    };
+    const homeStack = () => {
+      return (
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={homeTab} />
+        </Stack.Navigator>
+      );
+    };
+    const homeTab = () => {
+      return (
+        <BottomTabs.Navigator initialRouteName="MyNote" backBehavior="none">
+          <BottomTabs.Screen name="MyNote" component={MyNote} />
+          <BottomTabs.Screen name="Map" component={Map} />
+          <BottomTabs.Screen name="Write" component={Write} />
+          <BottomTabs.Screen name="Post" component={Post} />
+          <BottomTabs.Screen name="Setting" component={Setting} />
+        </BottomTabs.Navigator>
+      );
+    };
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Login"
+          headerMode="false"
+          backBehavior="none">
+          <Stack.Screen name="Login" component={loginStack} />
+          <Stack.Screen name="Home" component={homeStack} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  };
+  return <Navigator />;
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
